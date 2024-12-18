2a0a7736dbce27c95092d0029721ff5e23f2476a
Rommel <garaisrommel04@gmail.com>
2024-12-18 23:16:57 +0800

    Rearranged the migration files
    Separated the migration files for doctors, patients, and appointments

---
+++ database/migrations/2024_12_18_145443_create_doctors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('doctor_id');
+            $table->foreign('doctor_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('specialization');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('doctors', function(Blueprint $table) {
+            $table->dropForeign('doctors_doctor_id_foreign');
+        });
+        
+        Schema::dropIfExists('doctors');
+    }
+};
