b83dd5bd238c22b634a82b19b0292f247c82611d
Rommel <garaisrommel04@gmail.com>
2024-12-21 23:12:41 +0800

    Added Tailwind CSS
    Added and configured Tailwind CSS

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,8 @@
 <head>
     <link rel="stylesheet" href="{{ asset('/css/global.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/header.css') }}">
+
+    @vite('resources/css/app.css')
 </head>
 
 @auth
@@ -9,13 +11,11 @@
         <div class="logo-navbar-container">
             <a href="/home"><h2>Brand</h2></a>
             <nav class="nav-bar">
-                
                     <ul class="list-container">
                         <li class="nav-list_item"><a class="nav-link nav-text" href="/home">Home</a></li>
                         <li class="nav-list_item"><a class="nav-link nav-text" href="/doctors">Doctors</a></li>
                         <li class="nav-list_item"><a class="nav-link nav-text" href="/appointments">Appointments</a></li>
                     </ul>
-                
             </nav>
         </div>
         <hr>
