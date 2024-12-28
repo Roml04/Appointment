de7c830baed5fdfafd4fcc6b81890e882b05c695
Lienzo <ik6027769@gmail.com>
2024-12-28 12:00:28 +0800

    Styled schedule page

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -7,30 +7,47 @@
     <title>Schedules</title>
     @vite('resources/css/app.css')
 </head>
-<body>
+<body class="flex gap-6">
     <x-sidebar/>
-    <h1>Schedule an Appointment</h1>
-    <form action="">
-        @csrf
-        <label for="appointment-type">Appointment Type</label><br>
-        <select name="" id="appointment-type">
-            <option value="general-consultation">General Consultation</option>
-            <option value="general-consultation">Emergency Appointment</option>
-            <option value="general-consultation">Follow-up Appointment</option>
-            <option value="general-consultation">Routine Check-up</option>
-            <option value="general-consultation">Diagnostic Appointment</option>
-            <option value="general-consultation">Health Screening Management</option>
-            <option value="general-consultation">Immunization Appointment</option>
-        </select><br>
+    <div class="py-6 w-full space-y-5">
+        <h1>Schedule an Appointment</h1>
+        <form class="space-y-2.5" action="" method="POST">
+            @csrf
+            <fieldset class="flex justify-between items-center h-10">
+                <label for="appointment-type">Appointment Type</label>
+                <select name="" id="appointment-type" required>
+                    <option value="general-consultation">General Consultation</option>
+                    <option value="general-consultation">Emergency Appointment</option>
+                    <option value="general-consultation">Follow-up Appointment</option>
+                    <option value="general-consultation">Routine Check-up</option>
+                    <option value="general-consultation">Diagnostic Appointment</option>
+                    <option value="general-consultation">Health Screening Management</option>
+                    <option value="general-consultation">Immunization Appointment</option>
+                </select>
+            </fieldset>
+    
+            <fieldset class="flex justify-between items-center h-10">
+                <label for="appointment-date">Date</label>
+                <input id="appointment-date" type="date" required>
+            </fieldset>
 
-        <label for="appointment-date">Date</label><br>
-        <input id="appointment-date" type="date"><br>
-        <label for="appointment_time">Time</label><br>
-        <input id="appointment_time" type="time"><br>
+            <fieldset class="flex justify-between items-center h-10">
+                <label for="appointment_time">Time</label>
+                <input id="appointment_time" type="time" required>
+            </fieldset>
 
-        <label for="additional-notes">Notes (Optional)</label><br>
-        <textarea name="" id="additional-notes" cols="30" rows="10"></textarea><br>
-        <input type="submit" value="Schedule Appointment">
-    </form>
+            <hr>
+            
+            <fieldset>
+                <label for="additional-notes">Notes (Optional)</label><br>
+                <textarea class="w-full" name="" id="additional-notes" cols="30" rows="10"></textarea><br>
+            </fieldset>
+
+            {{-- component --}}
+            <fieldset class="flex justify-end">
+                <input class="rounded-full py-2.5 px-5 bg-blue-500 text-white" type="submit" value="Schedule Appointment">
+            </fieldset>
+        </form>
+    </div>
 </body>
 </html>
